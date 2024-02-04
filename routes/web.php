e36b893adac7382c0e0a9172c6c02e0a86bf5593
Yusuf Osama <yosef.eldesoky@gmail.com>
2024-02-04 12:28:48 +0700

    added Chirp model

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ChirpController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -15,6 +16,9 @@ use Inertia\Inertia;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::resource('chirps', ChirpController::class)
+    ->only(['index', 'store'])
+    ->middleware(['auth']);
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
